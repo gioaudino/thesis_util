9fbd15e9ea041f5d7757772c965c979f498f79d6
Giorgio Audino <gioaudino91@gmail.com>
2018-07-03 12:35:28 +0200

    Writing properties file

--- stream_arcs_to_k2_tree.cpp
+++ stream_arcs_to_k2_tree.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <sdsl/k2_tree.hpp>
 
+std::vector<std::string> split(const std::string &s, char delim);
+std::pair<unsigned int,unsigned long> get_nodes_arcs(const std::string basename);
 
 int main(int argc, char** argv){
     if(argc < 2){
@@ -20,10 +22,53 @@ int main(int argc, char** argv){
     sdsl::k2_tree<2> k2;
     k2 = sdsl::k2_tree<2>(arc_vector, max+1);
 
-    std::ofstream outfile(argv[2]);
+    std::ofstream outfile(argv[1]);
+    std::string prop(argv[1]);
+    prop.append(".properties");
 
     auto written = k2.serialize(outfile);
     outfile.close();
 
+    std::ofstream properties_out(prop);
+    auto nodes_arcs = get_nodes_arcs(argv[1]);
+    unsigned int nodes = nodes_arcs.first;
+    unsigned long arcs = nodes_arcs.second;
+
+    properties_out << "arcs=" << arcs << std::endl;
+    properties_out << "nodes=" << nodes << std::endl;
+
+    properties_out.close();
+
     std::cout << "Written " << written << " bytes: " << 8*written << " bits" << std::endl;
+}
+
+std::pair<unsigned int,unsigned long> get_nodes_arcs(const std::string basename){
+    std::string properties_file(basename);
+    properties_file.append(".properties");
+    std::ifstream properties(properties_file);
+
+    std::string line;
+    std::vector<std::string> elems;
+    unsigned long arcs;
+    unsigned int nodes;
+    while(properties >> line){
+        elems = split(line, '=');
+        if(elems[0].compare("arcs") == 0){
+            arcs = std::stol(elems[1]);
+        }
+        if(elems[0].compare("nodes") == 0){
+            nodes = std::stoi(elems[1]);
+        }
+    }
+    properties.close();
+    return std::make_pair(nodes, arcs);
+}
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::stringstream ss(s);
+    std::string item;
+    std::vector<std::string> elems;
+    while (std::getline(ss, item, delim)) {
+        elems.push_back(std::move(item));
+    }
+    return elems;
 }
