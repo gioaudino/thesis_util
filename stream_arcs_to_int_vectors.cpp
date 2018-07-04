08a9325d0c5649e98ad1df0498346e31a95d85f9
Giorgio Audino <gioaudino91@gmail.com>
2018-07-04 15:32:44 +0200

    Created new script to use int vectors as a middleman

---
+++ stream_arcs_to_int_vectors.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <sdsl/k2_tree.hpp>
+#include <sdsl/int_vector.hpp>
+#include <ctime>
+
+std::vector<std::string> split(const std::string &s, char delim);
+std::pair<unsigned int,unsigned long> get_nodes_arcs(const std::string basename);
+
+int main(int argc, char** argv){
+    const int int_len = 32;
+
+    if(argc < 3){
+        std::cout << "Usage: " << argv[0] << "<output> <original_basename>" << std::endl;
+        exit(1);
+    }
+
+    unsigned int nodes;
+    unsigned long arcs;
+    auto nodes_arcs = get_nodes_arcs(argv[2]);
+    nodes = nodes_arcs.first;
+    arcs = nodes_arcs.second;
+
+    sdsl::int_vector<int_len> x_vector, y_vector;
+    x_vector = sdsl::int_vector<int_len>(arcs, 0);
+    y_vector = sdsl::int_vector<int_len>(arcs, 0);
+
+    long unsigned int index = 0;
+    time_t now;
+
+    while (std::cin >> x >> y){
+        if(index % 1000000 == 0){
+            now = time(0);
+            std::cout << ctime(&now) << "\t" << index << "/" << arcs << " arcs - " << 100*index/arcs << "%%" << std::endl;
+        }
+        x_vector.set_int(index*int_len, x, int_len);
+        y_vector.set_int(index*int_len, y, int_len);
+    }
+
+    std::cout << "Streaming completed. Serializing vectors" << std::endl;
+
+    std::ofstream x_stream(argv[1]+".x");
+    std::ofstream y_stream(argv[1]+".y");
+
+    x_vector.serialize(x_stream);
+    y_vector.serialize(y_stream);
+
+    x_stream.close();
+    y_stream.close();
+
+    std::string prop(argv[1]);
+    prop.append(".properties");
+    std::ofstream properties_out(prop);
+
+    properties_out << "arcs=" << arcs << std::endl;
+    properties_out << "nodes=" << nodes << std::endl;
+
+    std::cout << "Written " << written << " bytes: " << 8*written << " bits - " << bpe << " bpe" << std::endl;
+    time_t t = time(0);
+    std::cout << "Finished at " << ctime(&t) << std::endl;
+}
+
+std::pair<unsigned int,unsigned long> get_nodes_arcs(const std::string basename){
+    std::string properties_file(basename);
+    properties_file.append(".properties");
+    std::ifstream properties(properties_file);
+
+    std::string line;
+    std::vector<std::string> elems;
+    unsigned long arcs;
+    unsigned int nodes;
+    while(properties >> line){
+        elems = split(line, '=');
+        if(elems[0].compare("arcs") == 0){
+            arcs = std::stol(elems[1]);
+        }
+        if(elems[0].compare("nodes") == 0){
+            nodes = std::stoi(elems[1]);
+        }
+    }
+    properties.close();
+    return std::make_pair(nodes, arcs);
+}
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::stringstream ss(s);
+    std::string item;
+    std::vector<std::string> elems;
+    while (std::getline(ss, item, delim)) {
+        elems.push_back(std::move(item));
+    }
+    return elems;
+}
