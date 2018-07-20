7843cdecd6ce561db3d0c75ddc7e741ba99d52cf
Giorgio Audino <gioaudino91@gmail.com>
2018-07-20 12:21:51 +0200

    Removed useless wrong line

--- run_k2tree.sh
+++ run_k2tree.sh
@@ -14,5 +14,4 @@ compile=$?
 cd $goto
 if [[ compile -eq 0 ]]; then
     java it.unimi.dsi.webgraph.ArcListASCIIGraph $2 >($1/stream_arcs_to_k2_tree.out $3 $2);
-    time $1/int_vectors_to_k2tree.out $3 --no-output
 fi
