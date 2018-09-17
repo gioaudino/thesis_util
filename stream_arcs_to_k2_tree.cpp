f8e3db17e067f5fd79d7ecdc7a1289c41ee3afb5
Giorgio Audino <gioaudino91@gmail.com>
2018-09-17 17:48:17 +0200

    Testing binary search

--- stream_arcs_to_k2_tree.cpp
+++ stream_arcs_to_k2_tree.cpp
@@ -134,6 +134,7 @@ int main(int argc, char** argv){
 
     std::vector<int> test(out_degrees.size(), 0);
     for(int t = 0; t < 1000000; t++){
+        if(t % 1000 == 0) std::cout << "..." <<std::endl;
         int selected_node = get_proportionally_random_node(out_degrees);
         test[selected_node]++;
     }
@@ -148,7 +149,7 @@ int main(int argc, char** argv){
     std::cout << "Result: ";
 
     for (int t = 0; t < out_degrees.size(); t++){
-        std::cout << (double) test[t]/1000000;
+        std::cout << (double) test[t]/1000000; << "\t";
     }
     std::cout << std::endl;
 
