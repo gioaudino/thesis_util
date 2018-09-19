94607d85e77cac48ad5e25aa29512afc87debcbf
Giorgio Audino <gioaudino91@gmail.com>
2018-09-19 10:37:50 +0200

    Added clear command

--- run_k2tree.sh
+++ run_k2tree.sh
@@ -13,5 +13,6 @@ b++ stream_arcs_to_k2_tree.cpp
 compile=$?
 cd $goto
 if [[ compile -eq 0 ]]; then
+    clear;
     java it.unimi.dsi.webgraph.ArcListASCIIGraph $2 >($1/stream_arcs_to_k2_tree.out $3 $2);
 fi
