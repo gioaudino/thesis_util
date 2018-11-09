d9db2f2ea177ff8131c1b2f168ab0d5bd3b6aec3
Giorgio Audino <gioaudino91@gmail.com>
2018-11-09 12:01:19 +0100

    Added big graphs for testing

--- run_webgraph_tests.sh
+++ run_webgraph_tests.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-graphs=( "enwiki-2015" "enwiki-2016" "enwiki-2017" "enwiki-2018" "eu-2015-host" "facebook" "gsh-2015-host" "twitter-2010" "uk-2014-host")
+graphs=( "enwiki-2015" "enwiki-2016" "enwiki-2017" "enwiki-2018" "eu-2015-host" "facebook" "gsh-2015-host" "twitter-2010" "uk-2014-host" "eu-2015" "clueweb12" "uk-2014")
 algorithms=( "BVGraph" "EFGraph")
 WD=$(pwd)
 
