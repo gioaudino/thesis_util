0e4fe2bdd4839ee3178f1312c52ed4b8d59311c6
Giorgio Audino <gioaudino91@gmail.com>
2018-11-09 11:45:41 +0100

    Removed comma

--- run_webgraph_tests.sh
+++ run_webgraph_tests.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 graphs=( "enwiki-2015" "enwiki-2016" "enwiki-2017" "enwiki-2018" "eu-2015-host" "facebook" "gsh-2015-host" "twitter-2010" "uk-2014-host")
-algorithms=( "BVGraph", "EFGraph")
+algorithms=( "BVGraph" "EFGraph")
 WD=$(pwd)
 
 for graph in "${graphs[@]}"
